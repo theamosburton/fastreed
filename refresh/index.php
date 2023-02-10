34356d0fbb2f622d5a42d4b786c3f798c6cf7499
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-02-10 18:02:48 +0530

    fdgfhf

--- refresh/index.php
+++ refresh/index.php
@@ -9,8 +9,8 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-     echo "Git Hub Refresh"
-     $output = exec("git pull fastreed mai");
+     echo "Git Hub Refresh";
+     $output = exec("git pull fastreed main");
      echo "<pre>$output</pre>";
      ?>
   </body>
