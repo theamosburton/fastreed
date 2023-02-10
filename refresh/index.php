d2f54b36eebf627fa7f8eca2f0f44b849b4823bc
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-02-10 18:07:16 +0530

    tryty

--- refresh/index.php
+++ refresh/index.php
@@ -10,8 +10,18 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
      echo "Git Hub Refresh";
-     $output = exec("git pull fastreed main");
-     echo "<pre>$output</pre>";
+     function execPrint($command) {
+      $result = array();
+      exec($command, $result);
+      print("<pre>");
+      foreach ($result as $line) {
+          print($line . "\n");
+      }
+      print("</pre>");
+  }
+  // Print the exec output inside of a pre element
+  execPrint("git pull fastreed main");
+  execPrint("git status");
      ?>
   </body>
 </html>
