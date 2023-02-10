c960967cab2eea9eb9f6e7de21fe25ed23dfffe4
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-02-10 19:55:28 +0530

    gfhgfh

--- refresh/index.php
+++ refresh/index.php
@@ -6,8 +6,6 @@
   </head>
   <body>
     <?php
-    $old_path = getcwd();
-    chdir('/path/to/file');
     //make sure to make the shell file executeable first before running the shell_exec function
     $output = shell_exec('./shell-script.sh');
     chdir($old_path);
