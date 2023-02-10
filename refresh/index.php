4cb195c764a394d2c666f94ecdc93bf1ba43df9f
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-02-10 18:28:09 +0530

    fghgfh

--- refresh/index.php
+++ refresh/index.php
@@ -9,19 +9,9 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-     echo "Git Hub Refresh";
-     function execPrint($command) {
-      $result = array();
-      exec($command, $result);
-      print("<pre>");
-      foreach ($result as $line) {
-          print($line . "\n");
-      }
-      print("</pre>");
-    }
-     // Print the exec output inside of a pre element
-     execPrint("git pull https://github.com/mdshafiqmalik/Fastreed-v1.0.0 main");
-     execPrint("git status");
+
+    $output = shell_exec("git pull https://github.com/mdshafiqmalik/Fastreed-v1.0.0 main");
+    echo "<pre>$output</pre>"
      ?>
   </body>
 </html>
