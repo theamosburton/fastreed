a9e8595b08bedda496ae1b64dcda1fdcd805e18c
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-02-10 17:56:22 +0530

    dgfdg

--- refresh/index.php
+++ refresh/index.php
@@ -9,7 +9,7 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-     $output = shell_exec("git pull fastreed main");
+     $output = shell_exec("git pull fastreed mai");
      echo "<pre>$output</pre>";
      ?>
   </body>
