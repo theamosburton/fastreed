2814ca0bf9fe075851c5272c3ed2f12818f92841
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-02-11 18:06:07 +0530

    dfsgfd

--- assets/js/page.js
+++ assets/js/page.js
@@ -8,3 +8,12 @@ $("#toggle-icon").click(function(){
     }
 });
 
+$("#tab").click(function(){
+    if($("#tb-con").css('display') == 'block'){
+        $("#tb-con").hide(600);
+        $("#toggle-icon").css('transform', 'rotate(45deg)')
+    }else{
+        $("#tb-con").show(500);
+        $("#toggle-icon").css('transform', 'rotate(90deg)')
+    }
+});
