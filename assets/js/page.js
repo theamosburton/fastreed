a3d71444bbd8e779c4c974fb86c6a382c3db3c46
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-02-11 21:51:22 +0530

    Written Privay and Terms 1.0.0

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,13 +1,3 @@
-$("#toggle-icon").click(function(){
-    if($("#tb-con").css('display') == 'block'){
-        $("#tb-con").hide(600);
-        $("#toggle-icon").css('transform', 'rotate(45deg)')
-    }else{
-        $("#tb-con").show(500);
-        $("#toggle-icon").css('transform', 'rotate(90deg)')
-    }
-});
-
 $("#tab").click(function(){
     if($("#tb-con").css('display') == 'block'){
         $("#tb-con").hide(600);
