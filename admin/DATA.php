1da74dc88e81ee1f7db5392f3c0c283d105d7f30
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-02-15 13:59:21 +0530

    ghj

--- admin/DATA.php
+++ admin/DATA.php
@@ -3,8 +3,6 @@ $_SERVROOT = '../../';
 $GLOBALS['DB'] = $_SERVROOT.'/secrets/DB_CONNECT.php';
 include_once($GLOBALS['DB']);
 
-
-
 class getData{
     private $DB_CONNECT;
     function __construct()
