8cd9d61357d20244e596c46e738e6e60bbbf6e96
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-02-16 22:26:51 +0530

    dfgdgfdg

--- admin/index.php
+++ admin/index.php
@@ -93,6 +93,7 @@ if(!isset($_COOKIE['AID'])){
 
 						<label for="rows">Rows:</label>
 						<select name="" id="rows">
+						    <option value="5">5</option>
 						    <option value="10">10</option>
 						    <option value="25">25</option>
 							<option value="50">50</option>
