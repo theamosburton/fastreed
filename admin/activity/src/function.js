15a61461c4a5cbe93ee3d295fb7f8c77d0202d73
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-02-17 17:27:26 +0530

    dsdgf

--- admin/activity/src/function.js
+++ admin/activity/src/function.js
@@ -1,92 +1,23 @@
 // get anpnymous data onload
 
-$.post( "src/anonymous.php", {which : 'sessionVisits'}, function(data){
-    let today =  data.today;
-    let yesterday = data.yesterday;
-    let growth;
-    let pl;
-    let arrow;
-    let border;
-    if(yesterday === 0 || today === 0){
-        growth = 0;
-        pl = 'profit';
-        arrow = 'up'
-        border = "green";
-    }else{
-        if(yesterday > today){
-            growth = yesterday/today*100;
-            growth = '-'+growth;
-            growth = parseInt(growth);
-            pl = 'loss';
-            arrow = 'down'
-            border = "orangered";
-        }else{
-            growth = today/yesterday*100;
-            growth = parseInt(growth);
-            pl = 'profit';
-            arrow = 'up'
-            border = "green";
-        }
-    }
-    
-
-    $('#views').html(`
-    <span class="number">${today}<span class="percent ${pl}">${growth}% </span><li class="${pl} rotate-${arrow} fa fa-play"></li></span>
-    <span class="last">${yesterday}</span>
-    <span class="entity">Visits</span>
-    `);
-    $('#views').css('border', `1px solid ${border}`);
-
-});
-
 
 
 $.post( "src/anonymous.php", {which : 'guests'}, function(data){
-    let today =  data.today;
-    let yesterday = data.yesterday;
-    let growth;
-    let pl;
-    let arrow;
-    let border;
-    if(yesterday === 0 || today === 0){
-        growth = 0;
-        pl = 'profit';
-        arrow = 'up'
-        border = "green";
-    }else{
-        if(yesterday > today){
-            growth = yesterday/today*100;
-            growth = '-'+growth;
-            growth = parseInt(growth);
-            pl = 'loss';
-            arrow = 'down'
-            border = "orangered";
-        }else{
-            growth = today/yesterday*100;
-            growth = parseInt(growth);
-            pl = 'profit';
-            arrow = 'up'
-            border = "green";
-        }
-    }
-    
-
-    $('#devices').html(`
-    <span class="number">${today}<span class="percent ${pl}">${growth}% </span><li class="${pl} rotate-${arrow} fa fa-play"></li></span>
-    <span class="last">${yesterday}</span>
-    <span class="entity">Devices</span>
-    `);
-    $('#devices').css('border', `1px solid ${border}`);
-
+    updateContent(data, 'devices');
 });
 
 $.post( "src/anonymous.php", {which : 'guests_sessions'}, function(data){
+    updateContent(data, 'sessions');
+});
+
+$.post( "src/anonymous.php", {which : 'sessionVisits'}, function(data){
+    updateContent(data, 'views');
+});
+
+function updateContent(data, viewId){
     let today = data.today;
     let yesterday = data.yesterday;
-    let growth;
-    let pl;
-    let arrow;
-    let border;
+    let growth, pl, arrow, border;
     if(yesterday === 0 || today === 0){
         growth = 0;
         pl = 'profit';
@@ -108,11 +39,11 @@ $.post( "src/anonymous.php", {which : 'guests_sessions'}, function(data){
             border = "green";
         }
     }
-
-    $('#sessions').html(`
+    $(`#${viewId}`).html(`
     <span class="number">${today}<span class="percent ${pl}">${growth}% </span><li class="${pl} rotate-${arrow} fa fa-play"></li></span>
     <span class="last">${yesterday}</span>
     <span class="entity">Sessions</span>
     `);
-    $('#sessions').css('border', `1px solid ${border}`);
-});
+    $(`#${viewId}`).css('border', `1px solid ${border}`);
+
+}
