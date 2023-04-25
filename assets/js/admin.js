d2ddc85c8967cd63601992f6a42ab55d6b0abd2c
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-04-25 19:45:29 +0530

    gjhj

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,7 +1,38 @@
 function hardRefresh(){
-
+    $('#HPicon').css('display', 'none');
+    $('#HRSpinner').css('display', 'block');
+    $('#refHard span').html('Pulling...');
+    const refreshUrl = `/.htactivity/REFRESH.php?intent=hardRefresh`;
+    refreshFunction(refreshUrl);
+    async function refreshFunction(url){
+        const response = await fetch(url);
+        var data = await response.json();
+        let isRefreshed = data.Result;
+        if (isRefreshed) {
+            $('#HRSpinner').css('display', 'none');
+            $('#HPicon').css('display', 'block');
+            $('#refHard span').html('Repo Pulled');
+        }else{
+            alert(data.message);
+        }
+    }
 }
 
 function refreshCss() {
-    
+    $('#RefreshIcon').css('display', 'none');
+    $('#RSpinner').css('display', 'block');
+    $('#refStyle span').html('Updating...');
+    const refreshUrl = `/.htactivity/REFRESH.php?intent=refreshCSS`;
+    refreshFunction(refreshUrl);
+    async function refreshFunction(url){
+        const response = await fetch(url);
+        var data = await response.json();
+        let isRefreshed = data.Result;
+        if (isRefreshed) {
+            $('#RSpinner').css('display', 'none');
+            $('#RefreshIcon').css('display', 'block');
+            $('#refStyle span').html('Updated');
+        }else{
+            alert(data.message);
+        }
 }
