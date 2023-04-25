b24d2c77d55e8d9210dc54deec429b4348a8771e
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-04-25 20:50:57 +0530

    gfhgh

--- .htactivity/REFRESH.php
+++ .htactivity/REFRESH.php
@@ -98,7 +98,7 @@ class  refreshSite{
 
     public function gitIsUpdated(){
         shell_exec('git fetch fastreed');
-        // Get the SHA hash of the latest commit on the loca
+        // Get the SHA hash of the latest commit on the
         $localSha = shell_exec('git rev-parse HEAD');
         $remoteSha = shell_exec('git rev-parse fastreed/main');
         // Compare the local and remote branches
