d4887237aaaf27399f07fa4e2cc135892f57de85
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-04-29 22:43:35 +0530

    fgf

--- .htactivity/API/getNotifications.php
+++ .htactivity/API/getNotifications.php
@@ -55,7 +55,6 @@ function responseNotifications($dPID){
     }
 
     $sql2 = "SELECT * FROM notifications WHERE reciever = '$dPID'";
-    echo $dPID;
 
     $result2 = mysqli_query($DB, $sql2);
     $notifications2 = array();
