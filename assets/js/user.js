5dbe3faa362c38b2d172d15bebbf6af1765b531c
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-05-07 22:04:25 +0530

    fghgf

--- assets/js/user.js
+++ assets/js/user.js
@@ -182,7 +182,7 @@ async function isUserlogged(){
                     srcImage = "/assets/img/favicon2.jpg";
                 }
                 notificationHTML += `
-                <div onclick="markRead(${notificationData[g].id}, ${aTag})" class="notification" id="notification${g+1}">
+                <div onclick="markRead("${notificationData[g].id}", "${aTag}")" class="notification" id="notification${g+1}">
                     <a>
                         <img class="image" src="${srcImage}">
                         <div class="body">
