5a75a84c7d8d37a36cc3c3d78f3b424cd8f9cd93
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-05-13 08:05:53 +0530

    gfhgfhfg

--- profile/src/style.js
+++ profile/src/style.js
@@ -1,34 +1,3 @@
-styleUpdate();
-function styleUpdate() {
-    // checking cookie mode
-    let colorMode;
-    let cookieExist = (document.cookie.match(/^(?:.*;)?\s*colorMode\s*=\s*([^;]+)(?:.*)?$/)||[,null])[1];
-    if (cookieExist != null) {
-        colorMode = str_obj(document.cookie).colorMode;
-    }else{
-        document.cookie = "colorMode=dark; max-age=31104000; path=/";
-        colorMode = 'true';
-    }
-
-    // setting mode with respect to cookie
-    if (colorMode == 'dark') {
-        enableDarkMode();
-    }else if(colorMode == 'light'){
-        enableLightMode();
-    }
-    updateNeeded();
-    $('#notifications::-webkit-scrollbar-thumb').css('background-color', 'transparent');
-}
-
-function enableDarkMode() {
-    $('body').css('background-color','rgb(32, 33, 35)');
-}
-
-function enableLightMode() {
-    $('body').css('background-color','rgb(255, 255, 255)');
-}
-
-
 
 function toggleEditProfile(){
     var editFieldStatus = document.querySelector("#editFieldsStatus").innerHTML;
