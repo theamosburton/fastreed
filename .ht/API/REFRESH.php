59c60e8e4f78e6f6c2065bca01411fe241eea575
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-05-20 18:29:16 +0530

    ggh

--- .ht/API/REFRESH.php
+++ .ht/API/REFRESH.php
@@ -88,16 +88,17 @@ class  refreshSite{
     }
 
 
-public function hardRefresh(){
-    ob_start();
-    // Execute the shell command and capture the output
-    exec('git pull fastreed main', $output, $returnCode);
-    ob_end_clean(); 
-    if ($returnCode === 0) {
-        showError(true, "Updated Now");
-        
-    } else {
-        showError(false, "Not Updated");
+    public function hardRefresh(){
+        ob_start();
+        // Execute the shell command and capture the output
+        exec('git pull fastreed main', $output, $returnCode);
+        ob_end_clean(); 
+        if ($returnCode === 0) {
+            showError(true, "Updated Now");
+            
+        } else {
+            showError(false, "Not Updated");
+        }
     }
 }
 
