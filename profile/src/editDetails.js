add9535143399835ee37bb56e2a47c37f52460ba
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-05-20 14:24:54 +0530

    fghgfhg

--- profile/src/editDetails.js
+++ profile/src/editDetails.js
@@ -26,7 +26,6 @@ class updateDetails{
   }
 
   validateName() {
-    console.log("h");
     var nameRegex =/^[a-zA-Z]+(?: [a-zA-Z]+)*(?:\. [a-zA-Z]+)?$/;
     this.fullName = document.querySelector('#fullName').value;
     var uError = document.querySelector('#nameErrorMessage');
