19d3ea0c4748844c5e35beba1c0626d3bf667a7d
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-06-12 22:36:50 +0530

    ghfhfgh

--- .ht/controller/LOGGED_DATA.php
+++ .ht/controller/LOGGED_DATA.php
@@ -205,12 +205,17 @@ class getLoggedData{
         $sql = "SELECT * FROM followOthers WHERE follower = '$follweeUID' and followee = '$selfUID' and followBack = 1";
         $result = mysqli_query($this->DB, $sql);
 
+        
+
         // If this person is followed second person firstly
         // then he follows back
         $sql1 = "SELECT * FROM followOthers WHERE follower = '$selfUID' and followee = '$follweeUID'";
         $result1 = mysqli_query($this->DB, $sql1);
 
-        if (mysqli_num_rows($result) || mysqli_num_rows($result1)) {
+        $sql2 = "SELECT * FROM followOthers WHERE follower = '$selfUID' and followee = '$follweeUID' and followBack = 1";
+        $result2 = mysqli_query($this->DB, $sql2);
+
+        if (mysqli_num_rows($result) || mysqli_num_rows($result1) || mysqli_num_rows($result2)) {
             $return = true;
         }
        
