6721078a182adca20919708168765329da2ccaa7
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-06-13 14:55:53 +0530

    dfsfsf

--- profile/src/user.js
+++ profile/src/user.js
@@ -201,7 +201,7 @@ function follow(){
       if (data) {
         if (data.Result) {
           followButton.innerHTML = 'Followed';
-          followButton.onclick = 'unfollow()';
+          followButton.onclick.setAttribute( "onClick", "javascript: unfollow();");
         }else{
         followButton.innerHTML = `${data.message}`;
         }
@@ -234,7 +234,7 @@ function unfollow(){
       if (data) {
         if (data.Result) {
           followButton.innerHTML = 'Follow';
-          followButton.onclick = 'follow()';
+          followButton.onclick.setAttribute( "onClick", "javascript: follow();");
         }else{
           followButton.innerHTML = "Can't  unfollow";
         }
