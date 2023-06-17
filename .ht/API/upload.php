fdda757a2bdd90b09bcb405af614d54981d65054
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-06-17 09:11:17 +0530

    ggfhgfhfg

--- .ht/API/upload.php
+++ .ht/API/upload.php
@@ -59,8 +59,65 @@ class uploadMedia{
 
     }
 
-    private function uploadVideo($dPID){
-        $username = $this->userData->getOtherData('personID', $dPID)['username'];
+    private function uploadVideo($id){
+        $username = $this->userData->getOtherData('personID', $id)['username'];
+        if (!isset($_FILES['media'])) {
+            showMessage(false, 'No video Found');
+        }else{
+            $file = $_FILES['media'];
+            $file_tmp = $file['tmp_name'];
+            $file_error = $file['error'];
+            $file_ext = $_POST['ext'];
+            if ($file_error === UPLOAD_ERR_OK) {
+                $fileName = $this->BASIC_FUNC->createNewID("uploads" , "VID");
+                if($this->makeFileEntry($fileName, $username, $id, 'UP', 'videos', $file_ext)['Result']){
+                    $directory = $this->_DOCROOT.'/fastreedusercontent/videos/'.$username.'/';
+                    $add = '/fastreedusercontent/videos/'.$username.'/';
+                    // Create the directory if it doesn't exist
+                    if (!is_dir($directory)) {
+                        mkdir($directory, 0777, true);
+                    }
+                    $fileAddress = $directory.$fileName.'.'.$file_ext;
+                    $address = $add.$fileName.'.'.$file_ext;
+                    if (move_uploaded_file($file_tmp, $fileAddress)) {
+                        // File moved successfully
+                        showMessage(true, 'File Uploaded');
+                    } else {
+                        // Failed to move the file
+                        showMessage(false, 'Error moving the file');
+                    }
+                }else {
+                    showMessage(false, 'Video cannot entered in DB');
+                }
+            }else{
+                switch ($file_error) {
+                    case UPLOAD_ERR_INI_SIZE:
+                        showMessage(false, 'The uploaded file exceeds the upload_max_filesize directive in php.ini');
+                        break;
+                    case UPLOAD_ERR_FORM_SIZE:
+                        showMessage(false, 'The uploaded file exceeds the MAX_FILE_SIZE directive specified in the HTML form');
+                        break;
+                    case UPLOAD_ERR_PARTIAL:
+                        showMessage(false, 'The uploaded file was only partially uploaded');
+                        break;
+                    case UPLOAD_ERR_NO_FILE:
+                        showMessage(false, 'No file was uploaded');
+                        break;
+                    case UPLOAD_ERR_NO_TMP_DIR:
+                        showMessage(false, 'Missing a temporary folder');
+                        break;
+                    case UPLOAD_ERR_CANT_WRITE:
+                        showMessage(false, 'Failed to write file to disk');
+                        break;
+                    case UPLOAD_ERR_EXTENSION:
+                        showMessage(false, 'A PHP extension stopped the file upload');
+                        break;
+                    default:
+                        showMessage(false, 'Unknown error occurred during file upload');
+                        break;
+                }
+            }
+        }
     }
 
     private function uploadDP($id){
@@ -92,10 +149,10 @@ class uploadMedia{
                     }
                 }else {
                     showMessage(false, 'File cannot entered in DB');
-                  }
-              }else {
+                }
+            }else {
                 showMessage(false, 'Problem with image');
-              }
+            }
         }else {
             showMessage(false, 'No Image Found');
         }
