aeb530e8024fb4419df7099135489d69cb6f5215
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-06-20 17:02:39 +0530

    dfgfdg

--- uploads/index.php
+++ uploads/index.php
@@ -81,7 +81,6 @@ class getFastreedContent {
         $filepath =$this->_DOCROOT.'/.ht/fastreedusercontent/'.$type.'/'.$username.'/'.$IMGID.'.'.$EXT;
         echo $filepath;
 
-        var_dump(file_exists('/var/www/.ht/fastreedusercontent/photos/faastreed/IMG202306200000000.jpeg'));
         if (file_exists($filepath)) {
             $return = $filepath;
         }
