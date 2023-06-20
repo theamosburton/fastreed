db598cfdd94f05dfe695ce27ec1d1dcfc0e74169
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-06-20 16:51:02 +0530

    dfgfdgf

--- ind.php
+++ ind.php
@@ -4,5 +4,5 @@ $filepath = $_SERVER['DOCUMENT_ROOT'].'/assets/img/port13.png';
 header('Content-Type: image/png');
 header('Content-Length: ' . filesize($filepath));
 header('Content-Disposition: inline'); // Set to inline instead of attachment
-readfile($filepath);
+// readfile($filepath);
 ?>
