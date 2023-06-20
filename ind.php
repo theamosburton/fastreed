035b6af15fac29ceeec458a2c0481378e4484c77
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-06-20 18:03:35 +0530

    dfgdfgf

--- ind.php
+++ ind.php
@@ -1,7 +1,8 @@
 <?php
 $filepath = $_SERVER['DOCUMENT_ROOT'].'/.ht/fastreedusercontent/photos/faastreed/IMG202306200000000.jpeg';
-header('Content-Type: image/png');
-header('Content-Length: ' . filesize($filepath));
-header('Content-Disposition: inline'); // Set to inline instead of attachment
-readfile($filepath);
+echo $filepath;
+// header('Content-Type: image/png');
+// header('Content-Length: ' . filesize($filepath));
+// header('Content-Disposition: inline'); // Set to inline instead of attachment
+// readfile($filepath);
 ?>
