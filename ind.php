d40a67056238401ac6899ae6f86a75716f9eb341
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-06-20 16:41:22 +0530

    gfdgdfg

--- ind.php
+++ ind.php
@@ -1,5 +1,5 @@
 <?php
-$filepath = 'assets/img/port13.png';
+$filepath = $this->_DOCROOT.'/assets/img/port13.png';
 header('Content-Type: image/png');
 header('Content-Length: ' . filesize($filepath));
 header('Content-Disposition: inline'); // Set to inline instead of attachment
--- uploads/index.php
+++ uploads/index.php
@@ -56,6 +56,7 @@ class getFastreedContent {
                 }elseif ($type == 'videos') {
                     $contentType = 'video/'.$EXT;
                 }
+                
                 header('Content-Type: '.$contentType);
                 header('Content-Length: ' . filesize($filepath));
                 header('Content-Disposition: inline'); // Set to inline instead of attachment
