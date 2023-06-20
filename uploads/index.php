36f6c8d02751a48df7fed8fdb0af2490d2ebf379
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-06-20 19:25:01 +0530

    dfgdfgf

--- uploads/index.php
+++ uploads/index.php
@@ -57,7 +57,7 @@ class getFastreedContent {
                 }
                 header('Content-Type: '.$contentType);
                 header('Content-Length: ' . filesize($filepath));
-                header('Content-Disposition: inlinefilename=/favicon.ico');
+                header('Content-Disposition: inline; filename=/favicon.ico');
                 ob_clean();
                 flush();
                 readfile($filepath);
