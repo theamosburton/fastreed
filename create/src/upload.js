0f79bc5b95f3386b6b67ad2fbee03e97f66ec5ec
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-07-25 18:11:18 +0530

    fghfgh

--- create/src/upload.js
+++ create/src/upload.js
@@ -217,7 +217,6 @@ class fetchMedia{
           self.allMediaIDs[i] = upData[i].mediaID;
         }
         if (self.totalMedia != self.uploadsCount) {
-          
           self.showUploads();
         }else{
           var refreshElement = document.getElementById('rotateRefresh');
@@ -232,7 +231,53 @@ class fetchMedia{
  }
 
 
- showUploads() {
+createImageElement(imgURL, ulink, u) {
+  this.uploadsData['upload' + u] = {};
+  this.uploadsData['upload' + u].orglink = ulink;
+  var uploadedDiv = document.getElementById('uploads');
+  uploadedDiv.innerHTML += `
+    <div draggable="true" class="uploadContent" id="media${u}" onclick="edits.selectMedia('${imgURL}', 'image', '${ulink}')">
+      <img src="${imgURL}" class="lazyload">
+    </div>
+  `;
+}
+
+createVideoElement(videoURL, ulink, u) {
+  this.uploadsData['upload' + u] = {};
+  this.uploadsData['upload' + u].orglink = ulink;
+  var uploadedDiv = document.getElementById('uploads');
+  uploadedDiv.innerHTML += `
+    <div draggable="true" class="uploadContent" id="media${u}" onclick="edits.selectMedia('${videoURL}', 'video', '${ulink}')">
+      <video class="lazyload">
+        <source src="${videoURL}" type="video/mp4">
+      </video>
+      <div class="fileInfo">
+        <svg aria-label="Clip" color="rgb(255, 255, 255)" fill="rgb(255, 255, 255)" height="18" role="img" viewBox="0 0 24 24" width="18">
+          <path d="..."></path>
+        </svg>
+      </div>
+    </div>
+  `;
+}
+
+async fetchAndDisplayMedia(u) {
+  var ulink = this.uploads['up' + u].link;
+  var type = this.uploads['up' + u].type;
+  
+  if (type == 'image') {
+    const response = await fetch(ulink);
+    const blob = await response.blob();
+    var imgURL = URL.createObjectURL(blob);
+    this.createImageElement.call(this, imgURL, ulink, u);
+  } else if (type == 'video') {
+    const response = await fetch(ulink);
+    const blob = await response.blob();
+    var videoURL = URL.createObjectURL(blob);
+    this.createVideoElement.call(this, videoURL, ulink, u);
+  }
+}
+
+showUploads() {
   return new Promise((resolve) => {
     var uploadedDiv = document.getElementById('uploads');
     uploadedDiv.innerHTML = '';
@@ -240,52 +285,8 @@ class fetchMedia{
     var promises = [];
 
     for (let u = 0; u < this.uploadsCount; u++) {
-      var ulink = this.uploads['up' + u].link;
-      var type = this.uploads['up' + u].type;
-
-      if (type == 'image') {
-        const fetchPhotos = async () => {
-          const response = await fetch(ulink);
-          const blob = await response.blob();
-          var imgURL = URL.createObjectURL(blob);
-          this.uploadsData['upload' + u] = {};
-          this.uploadsData['upload' + u].orglink = ulink;
-          var uploadedDiv = document.getElementById('uploads');
-          uploadedDiv.innerHTML += `
-            <div draggable="true" class="uploadContent" id="media${u}" onclick="edits.selectMedia('${imgURL}', 'image', '${ulink}')">
-                <img src="${imgURL}" class="lazyload" >
-            </div>
-            `;
-        };
-
-        promises.push(fetchPhotos());
-      } else if (type == 'video') {
-        const fetchVideos = async () => {
-          const response = await fetch(ulink);
-          const blob = await response.blob();
-          var videoURL = URL.createObjectURL(blob);
-          this.uploadsData['upload' + u] = {};
-          this.uploadsData['upload' + u].orglink = ulink;
-          var uploadedDiv = document.getElementById('uploads');
-          uploadedDiv.innerHTML += `
-                <div draggable="true" class="uploadContent" id="media${u}" onclick="edits.selectMedia('${videoURL}', 'video', '${ulink}')">
-                    <video class="lazyload">
-                        <source src="${videoURL}" type="video/mp4">
-                    </video>
-                    <div class="fileInfo">
-                      <svg aria-label="Clip" color="rgb(255, 255, 255)" fill="rgb(255, 255, 255)" height="18" role="img" viewBox="0 0 24 24" width="18">
-                      <path d="m12.823 1 2.974 5.002h-5.58l-2.65-4.971c.206-.013.419-.022.642-.027L8.55 1Zm2.327 0h.298c3.06 0 4.468.754 5.64 1.887a6.007 6.007 0 0 1 1.596 2.82l.07.295h-4.629L15.15 1Zm-9.667.377L7.95 6.002H1.244a6.01 6.01 0 0 1 3.942-4.53Zm9.735 12.834-4.545-2.624a.909.909 0 0 0-1.356.668l-.008.12v5.248a.91.91 0 0 0 1.255.84l.109-.053 4.545-2.624a.909.909 0 0 0 .1-1.507l-.1-.068-4.545-2.624Zm-14.2-6.209h21.964l.015.36.003.189v6.899c0 3.061-.755 4.469-1.888 5.64-1.151 1.114-2.5 1.856-5.33 1.909l-.334.003H8.551c-3.06 0-4.467-.755-5.64-1.889-1.114-1.15-1.854-2.498-1.908-5.33L1 15.45V8.551l.003-.189Z" fill-rule="evenodd"></path>
-                      </svg>
-                    </div>
-                </div>
-            `;
-        };
-
-        promises.push(fetchVideos());
-      }
+      promises.push(this.fetchAndDisplayMedia.call(this, u));
     }
-
-    
 
     Promise.all(promises).then(() => {
       this.totalMedia = this.uploadsCount;
@@ -295,6 +296,7 @@ class fetchMedia{
     });
   });
 }
+
 
 
 }
