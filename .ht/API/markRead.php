e65388a20e1b3c40792b6b3adfe450d5a27de858
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-07-26 22:19:35 +0530

    ghjghjgh

--- .ht/API/markRead.php
+++ .ht/API/markRead.php
@@ -4,20 +4,21 @@ if ($proceedAhead) {
     new markRead();
 }
 class markRead{
+    private $DB;
     function __construct(){
         if (isset($_GET) && !empty($_GET['SNO'])) {
             $sno = $_GET['SNO'];
             // checking if it is permanent or temprory
             $sql =  "SELECT * FROM notifications WHERE `s.no` = '$sno'";
             $DB_CONNECT = new Database();
-            $DB = $DB_CONNECT->DBConnection();
-            $result = mysqli_query($DB, $sql);
+            $this->DB = $DB_CONNECT->DBConnection();
+            $result = mysqli_query($this->DB, $sql);
             if ($result) {
                 $row = mysqli_fetch_assoc($result);
                 $status = $row['status'];
                 if ($status) {
                     $sql2 = "UPDATE notifications SET markRead = '1'  WHERE `s.no` = '$sno'";
-                    $result2 = mysqli_query($DB, $sql2);
+                    $result2 = mysqli_query($this->DB, $sql2);
                     if ($result2) {
                         showMessage(true, "Marked Read");
                     }else {
@@ -25,7 +26,7 @@ class markRead{
                     }
                 }else{
                     $sql3 = "DELETE FROM notifications WHERE `s.no` = '$sno'";
-                    $result3 = mysqli_query($DB, $sql3);
+                    $result3 = mysqli_query($this->DB, $sql3);
                     if ($result3) {
                         showMessage(true, "Deleted");
                     }else {
