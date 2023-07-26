fc075858b51c1d8e3d3c8fc2e0708ba971cd0ca3
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-07-26 22:06:19 +0530

    fghgfhgfh

--- uploads/index.php
+++ uploads/index.php
@@ -56,11 +56,11 @@ class getFastreedContent {
                 // echo $filepath;
                 header('Cache-Control: max-age=2592000'); // Cache for 1 hour
                 header('Expires: '.gmdate('D, d M Y H:i:s', time() + 2592000).' GMT'); // Cache for 1 hour
-                // header('Content-Type: '.$contentType);
+                header('Content-Type: '.$contentType);
                 header('Content-Length: ' . filesize($filepath));
                 header('Content-Disposition: inline; filename=favicon.ico');
-                ob_clean();
-                flush();
+                // ob_clean();
+                // flush();
                 readfile($filepath);
             }
         }
