9a05037290a4faa9b3f3233040c06f447d75e404
Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-10-14 19:35:20 +0530

    fghfgh

--- sitemap/index.php
+++ sitemap/index.php
@@ -11,7 +11,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
     <loc>'.DOMAIN.'/sitemap_authors.xml</loc>
   </sitemap>
   <sitemap>
-    <loc>'.DOMAIN.'/sitemap_webstories.xml.gz</loc>
+    <loc>'.DOMAIN.'/sitemap_webstories.xml</loc>
   </sitemap>
 </sitemapindex>';
 
