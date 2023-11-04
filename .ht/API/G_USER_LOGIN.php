0beef8697f3ec18841809b3db225cae7aad80378
Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-11-05 00:54:17 +0530

    gfdgdfgdf

--- .ht/API/G_USER_LOGIN.php
+++ .ht/API/G_USER_LOGIN.php
@@ -175,7 +175,7 @@ class gSignUpLogin{
 
   // This function is for logging out account
   public function logoutAccount(){
-    $avalFor = '.'.DOMAIN_NAME_ALIAS;
+    $avalFor = '.www.'.DOMAIN_NAME_ALIAS;
     setcookie('UID', "", time()-3600, '/' , $avalFor);
     setcookie('RMM',  "", time()-3600, '/' , $avalFor);
     unset($_SESSION['LOGGED_USER']);
