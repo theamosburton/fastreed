a14f0b93a4bd50d8fc52f4411e0ec8bba02abcf8
Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-11-07 19:35:46 +0530

    hgfhgh

--- assets/js/homepageLoader.js
+++ assets/js/homepageLoader.js
@@ -407,7 +407,7 @@ function showDialogueBox(storyID){
 }
 
 
-async function shareSupportedStory(title, des, url, image){
+async function shareSupportedStory(title, url, image){
   title = decodeURIComponent(title);
   var ext = image.split('.').pop();
   url = 'https://www.fastreed.com/webstories' + url;
