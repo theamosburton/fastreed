d52b5f40b0e9e25cfb5d1bf8b34b9e22dacb961e
Shafiq Malik <mdshafiqmalik98@gmail.com>
2023-11-10 17:24:18 +0530

    fghgfh

--- about/index.php
+++ about/index.php
@@ -24,6 +24,9 @@
     <link href="/assets/css/cropper.min.css?v=1.5.13" rel="stylesheet">
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-XZ4YQSPFM1"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2950653490323756"
+     crossorigin="anonymous">
+    </script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
